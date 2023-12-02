afbb8df3c73f6e3adae2551d5c1c317f93df93c0
sabnurovr <sabnurovv@mail.ru>
2023-12-02 17:45:18 +0600

    yui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+function PokemonList() {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/')
+        .then(response => response.json())
+        .then(data => {
+          setPokemonList(data.results);
+        })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>List</h1>
+        <ul>
+          {pokemonList.map((list, index) => (
+              <li key={index}>{list.name} <img src={list.url}/></li>
+          ))}
+        </ul>
+      </div>
   );
 }
 
-export default App;
+export default PokemonList;
+
